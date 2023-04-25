977478d45cc3358c9ef92737fd5c6bf17574e081
Vũ Đức Toản <vuductoan@mac.local>
2023-04-25 17:52:20 +0700

    Lab02 - 26/4 - Chua co diagram

---
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,77 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    // Cóntructor Methods
+
+    // Constructor Method 1: Create a DVD object by title
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+    // Constructor Method 2: Create a DVD object by category, title and cost
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    // Constructor Method 3: Create a DVD object by director, category, title and
+    // cost
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    // Constructor Method 4: Create a DVD object by all attributes: title, category,
+    // director, length and cost
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+}
