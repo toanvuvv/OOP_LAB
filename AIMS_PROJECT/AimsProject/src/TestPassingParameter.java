202d8ff09e7d1190dded4e2a273b74c5b0f2630b
Vũ Đức Toản <vuductoan@mac.local>
2023-05-27 09:15:10 +0700

    Điểm danh Lab03 - 27/5

---
+++ AIMS_PROJECT/AimsProject/src/TestPassingParameter.java
@@ -0,0 +1,25 @@
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(Object o1, Object o2) {
+        DigitalVideoDisc a = (DigitalVideoDisc) o1;
+        DigitalVideoDisc b = (DigitalVideoDisc) o2;
+        DigitalVideoDisc tmp = new DigitalVideoDisc(a.getTitle());
+        a.setTitle(b.getTitle());
+        b.setTitle(tmp.getTitle());
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        title = oldTitle;
+    }
+}
